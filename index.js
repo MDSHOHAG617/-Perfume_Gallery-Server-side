ab1453c1ae4c8cedfb815b2fe2dcf7c2836f41ac
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-30 02:58:36 +0600

    insert items

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-
 app.use(cors());
 app.use(express.json());
 
@@ -43,6 +42,14 @@ async function run() {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await itemsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //insert items
+    app.post("/items", async (req, res) => {
+      const newItems = req.body;
+      console.log("adding new items", newItems);
+      const result = await itemsCollection.insertOne(newItems);
       res.send(result);
     });
   } finally {
