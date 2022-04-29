7d34b1dcbd5f7eb79efb208a73112b0d6aabcb77
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-30 01:59:55 +0600

    delete done

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ async function run() {
       const items = await itemsCollection.findOne(query);
       res.send(items);
     });
+
+    //Delete a Product
+    app.delete("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await itemsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
