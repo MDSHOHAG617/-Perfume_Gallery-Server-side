486ad54b1c20b592110e9914d31e93d15aac0098
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-29 06:47:42 +0600

    items loaded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { listen } = require("express/lib/application");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,13 @@ async function run() {
       const query = {};
       const cursor = itemsCollection.find(query);
       const items = await cursor.toArray();
+      res.send(items);
+    });
+
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const items = await itemsCollection.findOne(query);
       res.send(items);
     });
   } finally {
