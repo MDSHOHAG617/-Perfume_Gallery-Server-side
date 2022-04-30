50a26cfc116c3fcb95b3a32235c8c1fba41b87dc
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-30 21:45:09 +0600

    deploying

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const ObjectId = require("mongodb").ObjectId;
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { listen } = require("express/lib/application");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +24,15 @@ async function run() {
   try {
     await client.connect();
     const itemsCollection = client.db("perfume_Gallery").collection("products");
+
+    //Jwt
+    app.post("/login", (req, res) => {
+      const email = req.body;
+      console.log(email);
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+      console.log(token);
+      res.send({ token });
+    });
 
     app.get("/items", async (req, res) => {
       const query = {};
@@ -48,6 +59,8 @@ async function run() {
     //insert items
     app.post("/items", async (req, res) => {
       const newItems = req.body;
+      // const tokenInfo = req.headers.authorization;
+      // console.log(tokenInfo);
       console.log("adding new items", newItems);
       const result = await itemsCollection.insertOne(newItems);
       res.send(result);
@@ -55,12 +68,7 @@ async function run() {
   } finally {
   }
 }
-
 run().catch(console.dir);
-
-//use middleware
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("running my node Server");
